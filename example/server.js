f35965cbdeb10fa1d6151223a4ace05511935949
unknown <eplatzer@qws-PC.dgeyc>
2015-02-25 16:43:43 -0300

    Cáscara vacía con ejemplos para servidor.

---
+++ example/server.js
@@ -0,0 +1,18 @@
+﻿var express = require('express');
+var app = express();
+
+var tabulator = require('..');
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
+function site_up(req,res){
+    res.send("<h1>tabulator demo</h1>");
+}
+
+app.get('/index.html',site_up);
+app.get('/',site_up);
+
+// app.use(tabulator());
+
--- test/test.js
+++ test/test.js
@@ -1,11 +1,44 @@
 
 var assert = require('assert');
-var sv = require('..');
+var tabulator = require('..');
 
 describe('tabulator', function(){
+    var internalCounter;
+
     describe('basic test', function(){
         it('first test', function(){
             throw new Error('no tests');
         });
     });
+
+    describe('server operations', function(){
+        var server;
+        before(function () {
+          server = createServer({
+            opts:'this opts'
+          });
+        });
+        it('should work ok', function(done){
+            request(server)
+            .get('/example/tabulator')
+            .expect(200, "works ok!")
+            .end(function(err, res){
+                if (err) return done(err);
+                assert.equal(internalCounter,1);
+                done()
+            });
+        });
+    });
 });
+
+
+function createServer(opts, fn) {
+    var _serve = tabulator(opts);
+    return http.createServer(function (req, res) {
+        fn && fn(req, res);
+        _serve(req, res, function (err) {
+            res.statusCode = err ? (err.status || 500) : 404;
+            res.end(err ? err.stack : 'sorry!');
+        });
+    });
+}
