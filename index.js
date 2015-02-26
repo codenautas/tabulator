4c01cc44dfe4d93ffcfee7335a96d381550ef0c4
unknown <eplatzer@qws-PC.dgeyc>
2015-02-26 11:21:14 -0300

    Primeros casos de prueba

--- index.js
+++ index.js
@@ -7,7 +7,23 @@
 /**
  * Module dependencies.
  */
+// import used by this file
+// var dependency = dependency || require('dependency');  
 
-exports = module.exports = function structureValidator(opts){
-    throw new Error("Not implemented yet!");
+var tabulator = {};
+
+(function(){
+
+tabulator.createSimpleCellFromString = function createSimpleCellFromString(text){
+    return {bad:'NOT IMPLEMENTED YET (1)!'}
 }
+
+tabulator.render = function render(matrix){
+    return 'NOT IMPLEMENTED YET (2)!';
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = tabulator;
+}
+})();
+
--- test/test.js
+++ test/test.js
@@ -1,33 +1,58 @@
-
+﻿
+var expect = require('expect.js');
 var assert = require('assert');
+var http = require('http');
+var path = require('path');
+var request = require('supertest');
 var tabulator = require('..');
 
 describe('tabulator', function(){
-    var internalCounter;
-
-    describe('basic test', function(){
-        it('first test', function(){
-            throw new Error('no tests');
+    describe('createSimpleCellFromString', function(){
+        it('should create an object with one property', function(done){
+            var simpleCell=tabulator.createSimpleCellFromString("hola 'che'. ¿tenés < de 5 años?");
+            expect(simpleCell).to.eql({show: "hola 'che'. ¿tenés < de 5 años?"});
+            // assert.equal(simpleCell,{show: "hola 'che'. ¿tenés < de 5 años?"});
         });
     });
-
-    describe('server operations', function(){
-        var server;
-        before(function () {
-          server = createServer({
-            opts:'this opts'
-          });
+    describe('render', function(){
+        var simple=tabulator.createSimpleCellFromString;
+        it('should render a 2x3 matrix into a html table', function(){
+            var matrix={
+                lines:[{
+                    cells:[simple("one"), simple("two"), simple("three")]
+                },{
+                    cells:["alpha", "betha", "gamma"].map(function(text){ return simple(text); })
+                }]
+            };
+            var html=tabulator.render(matrix,{pretty:true});
+            expect(html).to.eql(
+                "<table>\n"+
+                "  <tbody>\n"+
+                "    <tr>\n"+
+                "      <td>one</td>\n"+
+                "      <td>two</td>\n"+
+                "      <td>three</td>\n"+
+                "    </tr>\n"+
+                "    <tr>\n"+
+                "      <td>alpha</td>\n"+
+                "      <td>betha</td>\n"+
+                "      <td>gama</td>\n"+
+                "    </tr>\n"+
+                "  </tbody>\n"+
+                "</table>"
+            );
         });
-        it('should work ok', function(done){
-            request(server)
-            .get('/example/tabulator')
-            .expect(200, "works ok!")
-            .end(function(err, res){
-                if (err) return done(err);
-                assert.equal(internalCounter,1);
-                done()
-            });
+        it('should escape when render', function(){
+            var countCall2escape=0;
+            tabulator.escape=function(pureText){
+                countCall2escape++;
+                return "#"+pureText+"#";
+            }
+            var matrix={lines:[{cells:[simple("3<4 & 3>1")]}]};
+            var html=tabulator.render(matrix); // not pretty
+            expect(html).to.eql("<table><tbody><tr><td>#3<4 & 3>1#</td></tr></tbody></table>");
         });
+        
     });
 });
 
