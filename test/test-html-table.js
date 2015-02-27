cd4ba7fc0905b55817be68187269579fc9a3bef5
unknown <eplatzer@qws-PC.dgeyc>
2015-02-27 17:14:50 -0300

    Separo los casos de prueba en dos js distintos.

--- test/test-html-table.js
+++ test/test-html-table.js
@@ -1,41 +1,5 @@
 ﻿
 var expect = require('expect.js');
-var assert = require('assert');
-var http = require('http');
-var path = require('path');
-var request = require('supertest');
-var numeral = require('numeral');
-
-numeral.language('es-ar', {
-    delimiters: {
-        thousands: '.',
-        decimal: ','
-    },
-    abbreviations: {
-        thousand: 'm',
-        million: 'M',
-        billion: 'mM',
-        trillion: 'B'
-    },
-    ordinal : function (number) {
-        switch(number){
-            case 3:
-            case 1: return 'ro';
-            case 2: return 'do';
-            case 6:
-            case 5:
-            case 4: return 'to';
-            case 7: return 'mo';
-            case 8: return 'vo';
-            case 9: return 'no';
-        }
-        return '';
-    },
-    currency: {
-        symbol: '$'
-    }
-});
-
 
 var Tabulator = require('..');
 
@@ -132,14 +96,19 @@ describe('tabulator', function(){
             );
         });
     });
+    /*
     describe('toMatrix', function(){
-        it.skip('shoud matrix a list with three header variables and 1 data variable',function(){
+        it('shoud matrix a list with three header variables and 1 data variable and with holes',function(){
+            var countCall2toCell=0;
             var datum={
                 list:[
+                    {zone:'totalZ', area:'totalA', sex:'both', number:19000,total:19000},
+                    {zone:'totalZ', area:'totalA', sex:'masc', number:9745, total:19000},
                     {zone:'zone 1', area:'area 1', sex:'masc', number:5110, total:10000},
                     {zone:'zone 1', area:'area 1', sex:'fem' , number:4890, total:10000},
+                    {zone:'zone 1', area:'area 2', sex:'fem' , number:4365, total: 9000},
                     {zone:'zone 1', area:'area 2', sex:'masc', number:4635, total: 9000},
-                    {zone:'zone 1', area:'area 2', sex:'fem' , number:4365, total: 9000}
+                    {zone:'zone 1', area:'area 2', sex:'both' ,number:9000, total: 9000}
                 ],
                 vars:[
                     {name: 'zone'  , place: 'left'},
@@ -152,20 +121,46 @@ describe('tabulator', function(){
                     return data.number/total.number*100
                 }
             };
+            tabulator.toCell=function(row){
+                countCall2toCell++;
+                return {display:row.number/row.total, numerator:row.number, denominator:row.total};
+            }
             var matrix={
-                headers:[{
-                    "status": "not defined yet"
-                }],
+                lineVariables:['zone','area'],
+                columnVariables:['sex'],
+                columns:[
+                    {titles:['both']},
+                    {titles:['masc']},
+                    {titles:['fem' ]}
+                ],
                 lines:[{
-                    cells:["100", "51.1", "48.9"]
+                    titles:['totalZ', 'totalA'],
+                    cells:[
+                        {display:100  , number:19000,total:19000},
+                        {display:52   , number: 9880,total:19000},
+                        null
+                    ]
                 },{
-                    cells:["100", "51.5", "48.5"]
+                    titles:['zone 1', 'area 1'],
+                    cells:[
+                        null,
+                        {display:51.1 , number:5110, total:10000},
+                        {display:48.9 , number:4890, total:10000}
+                    ]
+                },{
+                    titles:['zone 1', 'area 2'],
+                    cells:[
+                        {display:100  , number:9000, total: 9000},
+                        {display:51.5 , number:4635, total: 9000},
+                        {display:48.5 , number:4365, total: 9000}
+                    ]
                 }]
             };
             var obtain=tabulator.toMatrix(datum);
             expect(obtain).to.eql(matrix);
         });
     })
+    */
 });
 
 
