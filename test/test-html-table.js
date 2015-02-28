dd534a0ae430ee74785d364d99b37b6778ddfbe9
Emilio <emilioplatzer@gmail.com>
2015-02-28 16:45:27 -0300

    Completo el primer caso de pruebas de render de títulos y headers

--- test/test-html-table.js
+++ test/test-html-table.js
@@ -46,50 +46,67 @@ describe('tabulator', function(){
             expect(html).to.eql('<table><tbody><tr><TD attr=this>that</TD></tr></tbody></table>');
             expect(countCall2toHtmlTd).to.eql(1);
         });
-        it.skip('should render titles', function(){
+        it.skip('should render headers', function(){
             var matrix={
-                caption:"The caption",
-                headers:[{
-                    "status": "not defined yet"
-                }],
-                lines:[{
-                    cells:["100", "51.1", "48.9"]
-                },{
-                    cells:["100", "51.5", "48.5"]
-                }]
+                caption:"Data for zone and area by sex",
+                lineVariables:['zone','area'],
+                columnVariables:['sex'],
+                columns:[
+                    {titles:['both']},
+                    {titles:['masc']},
+                    {titles:['fem' ]}
+                ]
             };
             var html=tabulator.toHtmlTable(matrix,{pretty:true});
-            expect(html).to.eql(
-                "<table>\n"+
-                "  <caption>The caption</caption>\n"+
+            expect(html).to.contain("<table>\n"+
+                "  <caption>Data for zone and area by sex</caption>\n"+
+                "  <colgroup class=headers>\n"+
+                "    <col class=zone>\n"+
+                "    <col class=area>\n"+
+                "  </colgroup>\n"+
+                "  <colgroup class=data>\n"+
+                "    <col class='{\"sex\":\"both\"}'>\n"+
+                "    <col class='{\"sex\":\"masc\"}'>\n"+
+                "    <col class='{\"sex\":\"fem\"}'>\n"+
+                "  </colgroup>\n"+
                 "  <thead>\n"+
                 "    <tr>\n"+
-                "      <th></th>\n"+
-                "      <th></th>\n"+
-                "      <th></th>\n"+
-                "      <th colspan=2>sex</th>\n"+
+                "      <th rowspan=2>zone</th>\n"+
+                "      <th rowspan=2>area</th>\n"+
+                "      <th colspan=3>sex</th>\n"+ //colspan=3 porque columns.length=3
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "      <th>zone</th>\n"+
-                "      <th>area</th>\n"+
-                "      <th>Total</th>\n"+
+                "      <th>both</th>\n"+
                 "      <th>masc</th>\n"+
                 "      <th>fem</th>\n"+
                 "    </tr>\n"+
                 "  </thead>\n"+
+                "  <tbody>\n"
+            );
+        });
+        it.skip('should render line titles', function(){
+            var matrix={
+                lines:[
+                    { titles:['one','alpha','a'], cells:["101", "102"]},
+                    { titles:['two','betha','b'], cells:["103", "104"]}
+                ]
+            };
+            var html=tabulator.toHtmlTable(matrix,{pretty:true});
+            expect(html).to.contain(
                 "  <tbody>\n"+
                 "    <tr>\n"+
-                "      <th rowspan=2>zone 1</th>\n"+
-                "      <th>area 1</th>\n"+
-                "      <td>one</td>\n"+
-                "      <td>two</td>\n"+
-                "      <td>three</td>\n"+
+                "      <th>one</th>\n"+
+                "      <th>alpha</th>\n"+
+                "      <th>a</th>\n"+
+                "      <td>101</td>\n"+
+                "      <td>102</td>\n"+
                 "    </tr>\n"+
                 "    <tr>\n"+
-                "      <th>area 2</th>\n"+
-                "      <td>alpha</td>\n"+
-                "      <td>betha</td>\n"+
-                "      <td>gama</td>\n"+
+                "      <th>two</th>\n"+
+                "      <th>betha</th>\n"+
+                "      <th>b</th>\n"+
+                "      <td>103</td>\n"+
+                "      <td>104</td>\n"+
                 "    </tr>\n"+
                 "  </tbody>\n"+
                 "</table>\n"
